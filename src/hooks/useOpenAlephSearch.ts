72dfb3fa7a77bf1556e0db69ba7fcc241cd124df
Moritz Neeb <nt4u@kpvn.de>
2026-08-11 02:03:32 +0200

    refactor: Create a hook to perform the OpenAleph search
    This was done with the help of LLM coding tools

---
+++ src/hooks/useOpenAlephSearch.ts
@@ -0,0 +1,74 @@
+import { useState, useCallback, useRef } from 'react';
+import { App, Notice } from 'obsidian';
+import {
+	SearchEndpoint,
+	default as openAlephClientFactory,
+	type OpenAlephPluginSettings,
+	type FederatedSearchResults,
+	type FacetState,
+	SCHEMA_TYPES,
+} from '../openaleph';
+
+export function useOpenAlephSearch(
+	pluginSettings: OpenAlephPluginSettings,
+	app: App,
+) {
+	const [searchResults, setSearchResults] = useState<FederatedSearchResults>();
+	const [facets, setFacets] = useState<FacetState>(() =>
+		Object.fromEntries(SCHEMA_TYPES.map((k) => [k, false])),
+	);
+
+	// TODO: get the actual labels from FtM
+	const facetLabels = Object.fromEntries(SCHEMA_TYPES.map((k) => [k, k]));
+
+	const clientRef = useRef(
+		(() => {
+			const Factory = openAlephClientFactory();
+			return new Factory(pluginSettings, app);
+		})(),
+	);
+
+	const runSearch = useCallback(
+		async (query: string) => {
+			const search = new SearchEndpoint(query);
+			for (const [key, active] of Object.entries(facets)) {
+				if (active) search.filter(key);
+			}
+			try {
+				const results = await clientRef.current.search(search);
+				setSearchResults(results);
+			} catch (err) {
+				console.error(err);
+				// eslint-disable-next-line obsidianmd/ui/sentence-case -- This is in proper sentence case.
+				new Notice('OpenAleph search failed. See log for details');
+			}
+		},
+		[facets],
+	);
+
+	const loadMore = useCallback(
+		async (instanceId: string) => {
+			if (searchResults !== undefined) {
+				await clientRef.current.loadMoreForInstance(
+					setSearchResults,
+					searchResults,
+					instanceId,
+				);
+			}
+		},
+		[searchResults],
+	);
+
+	const handleFacetToggle = useCallback((key: string, value: boolean) => {
+		setFacets((prev) => ({ ...prev, [key]: value }));
+	}, []);
+
+	return {
+		searchResults,
+		facets,
+		facetLabels,
+		runSearch,
+		loadMore,
+		handleFacetToggle,
+	};
+}
--- src/views/SearchSidebar.tsx
+++ src/views/SearchSidebar.tsx
@@ -1,26 +1,16 @@
-import { useState } from 'react';
-import { App, Notice } from 'obsidian';
+import { App } from 'obsidian';
 import { SearchView } from './SearchView';
 import { SearchResults } from './SearchResults';
 import OpenAlephPlugin from '../main';
 import {
-	SearchEndpoint as OpenAlephSearch,
-	default as openAlephClientFactory,
-	OpenAlephEntity,
+	type OpenAlephEntity,
+	type OpenAlephPluginSettings,
 	writeNote,
 	yamlifyEntity,
 	entityImportPath,
-	OpenAlephPluginSettings,
-	FederatedSearchResults,
-	SCHEMA_TYPES as FTM_SCHEMA_TYPES,
 } from '../openaleph';
 import { ConfirmNoteModal } from '../modals';
-
-const initialFacets = Object.fromEntries(
-	FTM_SCHEMA_TYPES.map((k) => [k, false]),
-);
-// TODO: get the actual labels from FtM
-const facetLabels = Object.fromEntries(FTM_SCHEMA_TYPES.map((k) => [k, k]));
+import { useOpenAlephSearch } from '../hooks/useOpenAlephSearch';
 
 export const SearchSidebar = ({
 	pluginSettings,
@@ -31,47 +21,14 @@ export const SearchSidebar = ({
 	app: App;
 	plugin: OpenAlephPlugin;
 }) => {
-	const [searchResults, setSearchResults] =
-		useState<FederatedSearchResults>();
-	const [facets, setFacets] = useState(initialFacets);
-	async function runSearch(query: string) {
-		console.log(`running search with query: "${query}"`);
-		const ClientFactory = openAlephClientFactory();
-		const apiClient = new ClientFactory(pluginSettings, app);
-		const search = new OpenAlephSearch(query);
-		const chosenFacets = Object.entries(facets).filter(
-			([_key, value]) => value,
-		);
-		console.log({ chosenFacets });
-		for (const [key, _val] of chosenFacets) {
-			search.filter(key);
-		}
-		console.log(search);
-		try {
-			const results = await apiClient.search(search);
-			setSearchResults(results);
-		} catch (err) {
-			console.error(err);
-			// eslint-disable-next-line obsidianmd/ui/sentence-case -- This is in proper sentence case.
-			new Notice('OpenAleph search failed. See log for details');
-		}
-	}
-
-	async function loadMore(instanceId: string) {
-		const ClientFactory = openAlephClientFactory();
-		const apiClient = new ClientFactory(pluginSettings, app);
-		if (searchResults !== undefined) {
-			await apiClient.loadMoreForInstance(
-				setSearchResults,
-				searchResults,
-				instanceId,
-			);
-		}
-	}
-
-	function handleFacetToggle(key: string, value: boolean) {
-		setFacets({ ...facets, [key]: value });
-	}
+	const {
+		searchResults,
+		facets,
+		facetLabels,
+		runSearch,
+		loadMore,
+		handleFacetToggle,
+	} = useOpenAlephSearch(pluginSettings, app);
 
 	return (
 		<>
